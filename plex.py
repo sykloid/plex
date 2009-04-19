02bb46ba4ca0a8e27d04ab612ba8c4c698f876de
P.C. Shyamshankar <sykora@lucentbeing.com>
2009-04-19 13:10:51 +0530

    Added the Rule class, to represent lexing rules.

--- plex.py
+++ plex.py
@@ -21,3 +21,31 @@ class Token(object) :
 
     def __str__(self) :
         return self.lexeme
+
+class Rule(object) :
+    '''A class representing rule for lexical analysis.
+
+    Each rule must have the following :
+        * A regular expression pattern which represents the rule.
+        * The token type given to all lexemes which match the pattern.
+
+    Additionally, a rule may also specify a callback function to be called when
+    the rule matches. This function is given the token's type, and the text that
+    matched.
+
+    Currently, the default callback is the constructor of the Token class, which
+    simply creates a token with the given attributes and returns it.
+
+    A callback of None can be used as a placeholder for a rule which is to
+    discard its token.
+    '''
+
+    def __init__(self, token_type, pattern, callback = Token) :
+        self.type = token_type
+        self.pattern = re.compile(pattern)
+        self.callback = callback
+
+    def __repr__(self) :
+        return '<Rule type = {0}, pattern = {1}, callback = {2}'.format(
+            self.type, self.pattern, self.callback
+        )
